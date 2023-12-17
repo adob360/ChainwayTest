71cc28181c5fbc6803414b9b6545895719676e9c
alexmac <a.dobler@mb-technologies.net>
2023-12-17 17:07:26 +0100

    Initial Commit

---
+++ ChainwayTest/ChainwayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChainwayTestApp.swift
+//  ChainwayTest
+//
+//  Created by Alex Dobler on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ChainwayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
